cd668a36f5ddaa0e40442392ff22a220e3073a7e
jake <jakenorg@outlook.com>
2024-01-14 15:47:27 -0600

    worked on some layout stuff

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def home():
+    if request.method == "GET":
+        return render_template("about.html")
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
